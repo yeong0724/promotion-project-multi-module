955ff8e776a780faf93d27ef651cb32c95b85fae
Jinyeong Kim <kid4211@gmail.com>
2026-08-20 22:35:23 +0900

    초기 프로젝트 생성

---
+++ src/main/java/com/jinyeong/promotionproject/PromotionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jinyeong.promotionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromotionProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinyeong/promotionproject/PromotionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinyeong.promotionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
